0a703fbd2f4b1fa21a0388a05b6047898d2527cb
adl <makara3416@gmail.com>
2024-04-26 08:51:25 +0200

    erstecommit

---
+++ src/main/java/com/greenrent/controller/ContactMessageController.java
@@ -0,0 +1,17 @@
+package com.greenrent.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // bu class controlldur dedik
+@RequestMapping("/contactmessage")  // mesaj ekleme cikarma icin
+public class ContactMessageController {
+	
+	//@PostMapping("/visitor")
+    //public ResponseEntity<Map<String,String>> createMessage(@Valid @RequestBody ContactMessage  contactMessage){
+		
+	//}
+}
---
+++ src/main/java/com/greenrent/domain/ContactMessage.java
@@ -0,0 +1,69 @@
+package com.greenrent.domain;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+
+
+@Entity
+@Table(name="tbl_cmessage")
+public class ContactMessage implements Serializable {  /**   //bu entiyiy networkda dolastirmak istersek eger bu  Serializable ozamn kullanilir
+
+*
+*/
+	private static final long serialVersionUID =1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Size(min=1,max=50,message="Your Name '{validatedValue}' must be between  {min} and {max} chars long")
+	@NotNull(message= "Please provide your name")
+	@Column(length = 50, nullable = false)
+	private String name;
+	
+	
+	@Size(min=5,max=50,message="Your subject '{validatedValue}' must be between  {min} and {max} chars long")
+	@NotNull(message= "Please provide message subject")
+	@Column(length = 50, nullable = false)
+	private String subject;
+	
+	
+	@Size(min=20,max=200,message="Your message body '{validatedValue}' must be between  {min} and {max} chars long")
+	@NotNull(message= "Please provide message body")
+	@Column(length = 50, nullable = false)
+	private String body;
+	
+	@Email
+	@Column(length = 50, nullable = false)
+	private String email;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
