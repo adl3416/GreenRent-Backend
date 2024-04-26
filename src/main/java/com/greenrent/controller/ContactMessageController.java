dad4d7574e4c34c6566116e332ce21171aecf580
adl <makara3416@gmail.com>
2024-04-26 09:11:08 +0200

    erstecommit

--- src/main/java/com/greenrent/controller/ContactMessageController.java
+++ src/main/java/com/greenrent/controller/ContactMessageController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+
+
 @RestController // bu class controlldur dedik
 @RequestMapping("/contactmessage")  // mesaj ekleme cikarma icin
 public class ContactMessageController {
 	
-	//@PostMapping("/visitor")
-    //public ResponseEntity<Map<String,String>> createMessage(@Valid @RequestBody ContactMessage  contactMessage){
+		@PostMapping("/visitor") //
+		public ResponseEntity<Map<String,String>> createMessage(@Valid @RequestBody ContactMessage  contactMessage){
 		
-	//}
+		}
 }
--- src/main/java/com/greenrent/domain/ContactMessage.java
+++ src/main/java/com/greenrent/domain/ContactMessage.java
@@ -11,13 +11,15 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 
 
 @Entity
