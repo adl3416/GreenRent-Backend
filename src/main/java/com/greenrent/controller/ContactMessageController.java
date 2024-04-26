e1e146866375c7f7c2d0bdfc84e941d122311c2f
adl <makara3416@gmail.com>
2024-04-26 12:10:37 +0200

    erstecommit

--- src/main/java/com/greenrent/controller/ContactMessageController.java
+++ src/main/java/com/greenrent/controller/ContactMessageController.java
@@ -3,8 +3,12 @@ package com.greenrent.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +26,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class ContactMessageController {
 		
+	    @Autowired
 		private ContactMessageService contactMessageService;   // service  bagliyoruz
 		
 		@PostMapping("/visitor")                                    // kisiyiyada mesaji kayitetmek icin @PostMapping kullanilir
@@ -33,6 +38,21 @@ public class ContactMessageController {
 			return new ResponseEntity<>(map,HttpStatus.OK);
 			
 		}
+		
+		@GetMapping  //getirmemizi sagliyor.
+		public ResponseEntity<List<ContactMessage>> getAllContactMessage(){
+			contactMessageService.getAllContactMessage(); // yukarida messageService(MessageService den) olusturduk. Ona e git  orda ben sana bir tane method yazcagim onu cagir gel.MessageService gidiyoruz orda getAllMessage olmadigi icin olusturmaliyiz
+			
+			List<Message> allMessage=	messageService.getAllMessage(); //repositoryden gelen responseentiy. Bana list döndü  buna ismm verdim allMessage
+//			return new ResponseEntity<>(allMessage,HttpStatus.OK);  2. yontem
+			return  ResponseEntity.ok(allMessage); // staticse . ok ile cagirabiliyoruz
+		}
+		
+		  @DeleteMapping("/{id}")
+		  public ResponseEntity<Map<String,String>> deleteContactMessage(@PathVariable Long id){ //id yi alabilmek icin @PathVariable  kullaniyoruz
+		  contactMessageService.deleteContactMessage(id);  //messagecomtactService git ve deleteContatMessage(id) yaz.Hangi meseji silceginide veriyoruz (id)-->ContactMesssageService clasina gönderdim silmek icin. MesssageService eye gidiyoruz.
+		  
+		  }
 		
 		
 }
