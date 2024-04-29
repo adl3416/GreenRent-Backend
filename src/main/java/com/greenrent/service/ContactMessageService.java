7bec3972a04e70ff39aa9f03177896ed058936e3
adl <makara3416@gmail.com>
2024-04-29 09:53:22 +0200

    v

--- src/main/java/com/greenrent/service/ContactMessageService.java
+++ src/main/java/com/greenrent/service/ContactMessageService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.greenrent.domain.ContactMessage;
 import com.greenrent.exception.ResourceNotFoundException;
+import com.greenrent.exception.message.ErrorMessage;
 import com.greenrent.repository.ContactMessageRepository;
 
 import lombok.AllArgsConstructor;
@@ -19,6 +20,8 @@ public class ContactMessageService {
 	@Autowired   // burdan ulasabilmemizi sagliyor
 	private ContactMessageRepository repository;   //service de repositorye baglaniyor
 	
+	
+	
 	       //buraya void de yazabiliriz ozaman bisey döndürmez simdi ContactMessage döndurcek
 	public ContactMessage createContactMessage(ContactMessage contactMessage) { // controllerden gönderilen messageyi aliyoruz controllerin istegi ile createContactMessage olusturuyorum.                                  
 		ContactMessage msg = repository.save(contactMessage);  // messageyi Repository e gönderip veri tabanina kayit ediyoruz
@@ -29,7 +32,13 @@ public class ContactMessageService {
 	
 	
 	public List<ContactMessage> getAllContactMessage(){  // ContactMessageController classindan buraya geldik getAllMessage yi olusturduk. Repositoory göndercez
-		return repository.findAll();   //repositoryy git ve findAll ile hepsi getir.List döndürcek
+		return repository.findAll();                     //repositoryy git ve findAll ile hepsi getir.List döndürcek
+	}
+	
+	
+	public  ContactMessage getContactMessage(Long id) { //messageRepository git findById(id) getir. Bulunan mesaji return foundMessage; ile geri gönder
+	    return  repository.findById(id).orElseThrow(()->
+	     new ResourceNotFoundException(String.format(ErrorMessage.RESOURCE_NOT_FOUND_MESSAGE,id)));
 	}
 	
 	
